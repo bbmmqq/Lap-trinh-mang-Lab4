c11868a32635ca82a01ae14fa5ccb9289df52a35
bbmmqq <buiquan0706@ggmail.com>
2024-05-27 22:15:11 +0700

    Lab4

---
+++ bai5/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace bai5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxurl = new System.Windows.Forms.TextBox();
+            this.txtBoxUsername = new System.Windows.Forms.TextBox();
+            this.txtBoxPass = new System.Windows.Forms.TextBox();
+            this.kq = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(649, 140);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(102, 64);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtBoxurl
+            // 
+            this.txtBoxurl.Location = new System.Drawing.Point(229, 24);
+            this.txtBoxurl.Name = "txtBoxurl";
+            this.txtBoxurl.Size = new System.Drawing.Size(138, 26);
+            this.txtBoxurl.TabIndex = 4;
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.Location = new System.Drawing.Point(229, 146);
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(138, 26);
+            this.txtBoxUsername.TabIndex = 5;
+            // 
+            // txtBoxPass
+            // 
+            this.txtBoxPass.Location = new System.Drawing.Point(229, 212);
+            this.txtBoxPass.Name = "txtBoxPass";
+            this.txtBoxPass.Size = new System.Drawing.Size(138, 26);
+            this.txtBoxPass.TabIndex = 6;
+            this.txtBoxPass.UseSystemPasswordChar = true;
+
+            // 
+            // kq
+            // 
+            this.kq.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kq.Cursor = System.Windows.Forms.Cursors.Default;
+            this.kq.Location = new System.Drawing.Point(1, 256);
+            this.kq.Name = "kq";
+            this.kq.Size = new System.Drawing.Size(797, 196);
+            this.kq.TabIndex = 7;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(649, 40);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(102, 64);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "SIGN UP";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_ClickAsync);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(229, 84);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(138, 26);
+            this.txtBoxEmail.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.kq);
+            this.Controls.Add(this.txtBoxPass);
+            this.Controls.Add(this.txtBoxUsername);
+            this.Controls.Add(this.txtBoxurl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxurl;
+        private System.Windows.Forms.TextBox txtBoxUsername;
+        private System.Windows.Forms.TextBox txtBoxPass;
+        private System.Windows.Forms.Label kq;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+    }
+}
+
---
+++ bai5/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace bai5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            var url =txtBoxurl.Text="https://nt106.uitiot.vn/auth/token";
+            var username = txtBoxUsername.Text;
+            var password = txtBoxPass.Text;
+
+            using (var client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent(username), "username" },
+                    { new StringContent(password), "password" }
+                };
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseString);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var detail = responseObject["detail"].ToString();
+                        kq.Text = $"Detail: {detail}";
+                        return;
+                    }
+
+                    var tokenType = responseObject["token_type"].ToString();
+                    var accessToken = responseObject["access_token"].ToString();
+                    kq.Text = $"Token Type: {tokenType}\nAccess Token: {accessToken}\n\nĐăng nhập thành công";
+                }
+                catch (Exception ex)
+                {
+                    kq.Text = $"Error: {ex.Message}";
+                }
+            }
+        }
+
+
+        private async void btnRegister_ClickAsync(object sender, EventArgs e)
+        {
+            var url = txtBoxurl.Text = "https://nt106.uitiot.vn/api/v1/user/signup";
+            var username = txtBoxUsername.Text;
+            var password = txtBoxPass.Text;
+            var email=txtBoxEmail.Text;
+
+            using (var client = new HttpClient())
+            {
+                var payload = new
+                {
+                    username = username,
+                    password = password,
+                    email= email,
+                };
+
+                var jsonPayload = JsonConvert.SerializeObject(payload);
+                var content = new StringContent(jsonPayload, System.Text.Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseObject = JObject.Parse(responseString);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        var detail = responseObject["detail"]?[0]?["msg"]?.ToString();
+                        kq.Text = $"Detail: {detail}";
+                        return;
+                    }
+
+                    kq.Text = "Đăng ký thành công!";
+                }
+                catch (Exception ex)
+                {
+                    kq.Text = $"Error: {ex.Message}";
+                }
+            }
+        }
+    }
+}
